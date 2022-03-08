19305a71a8ed820f77ba9c6c50538ea8f0a6e66b
P3mast <87942178+P3mast@users.noreply.github.com>
2022-03-08 21:38:43 +0100

    Add files via upload

---
+++ E-fox/script.js
@@ -0,0 +1,9 @@
+// https://github.com/mattboldt/typed.js/
+
+var typed = new Typed(".auto-input", {
+    strings: ["Moi", "Toi", "Vous"],
+    typeSpeed: 160,
+    backSpeed: 160,
+    loop: true,
+    cursorChar: ' 🦄 ',
+})
